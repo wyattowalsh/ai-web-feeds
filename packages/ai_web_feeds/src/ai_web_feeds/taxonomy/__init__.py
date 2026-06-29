bf8dd011a2ffced6a0f7b76f7a82f8f8f3db178b
wyattowalsh <wyattowalsh@gmail.com>
2026-06-29 04:40:54 -0400

    Revert "feat(taxonomy): restore taxonomy module with tests and visualization polish"
    This reverts commit 5dacf919124cb7b1eda26ce64ec2901ef17cc075.

--- packages/ai_web_feeds/src/ai_web_feeds/taxonomy/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Topic taxonomy loading and visualization."""
-
-from ai_web_feeds.taxonomy.loader import load_taxonomy
-from ai_web_feeds.taxonomy.models import Taxonomy, Topic
-from ai_web_feeds.taxonomy.visualizer import TaxonomyVisualizer
-
-__all__ = ["Taxonomy", "TaxonomyVisualizer", "Topic", "load_taxonomy"]
